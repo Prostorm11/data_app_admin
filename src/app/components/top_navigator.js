6a251a60d8625ada7604c843d104c6df89f9ebf5
Derrick Marfo <steammarfo111@gmail.com>
2025-09-16 00:44:04 +0200

    TopNavigatorMidComplete

--- src/app/components/top_navigator.js
+++ src/app/components/top_navigator.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./top_navigator.css"
+import CustomButton from './customButton'
 
 export default function TopNavigator() {
   return (
-    <div className='main_board'>
-      
+    <div className='main_board' style={{backgroundColor:"var(--card-background)",boxShadow:"0 4px 6px rgba(0, 0, 0, 0.1)"}}>
+      <CustomButton text={"Dashboard"}> </CustomButton>
+      <CustomButton text={"Clients"}> </CustomButton>
+      <CustomButton text={"Packages"}> </CustomButton>
+      <CustomButton text={"Orders"}> </CustomButton>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { darkTheme, lightTheme } from "./constants/colors";
 import TopNavigator from "./components/top_navigator";
+import "./css/page.css";
+import Profile from "./Screens/profile";
 
 export default function Home() {
-  const color=darkTheme
+  const color = darkTheme;
   return (
-    <div>
-      <h1>Welcome to Data Admin</h1>
+    <div className="main">
       <TopNavigator></TopNavigator>
+      <div style={{flex:1}}>
+        <Profile></Profile>
+      </div>
     </div>
   );
 }
